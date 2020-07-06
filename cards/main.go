b3266fdac4d030fb4feb132cc038eba0f322302d
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-07 02:27:09 +0530

    First commit

---
+++ cards/main.go
@@ -0,0 +1,33 @@
+// package main
+// import "fmt"
+// func main() {
+// 	// var card string = "Ace of Spades"
+// 	card := "Ace of Spades"
+// 	fmt.Println(card)
+// 	card = "Five of Diamonds"
+// 	fmt.Println(card)
+// }
+
+package main
+
+func main() {
+	// // // cards := deck{newCard(), "Ace of Diamonds"}
+	// // // cards = append(cards, "Six of spades")
+	// // // cards.print()
+	// // // fmt.Println(cards)
+	// // cards := newDeck()
+	// // // cards.print()
+	// // hand, remainingDeck := deal(cards, 5)
+	// // fmt.Println("Hand: ")
+	// // hand.print()
+	// // fmt.Println("Remaining Deck: ")
+	// // remainingDeck.print()
+	// greeting := "Hi, there."
+	// fmt.Println([]byte(greeting))
+	cards := 
+}
+
+// Function declaration in go
+func newCard() string {
+	return "Five of diamonds"
+}
