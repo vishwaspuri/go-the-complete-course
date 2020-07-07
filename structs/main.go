7f7a6a53ca2be12aaf15d4d795260a52d5655f61
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-08 02:49:13 +0530

    Pointerless implementation

--- structs/main.go
+++ structs/main.go
@@ -10,17 +10,28 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 12345,
 		},
 	}
-	fmt.Printf("%+v\n", jim)
+	jim.print()
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+// Recievers with structs
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
