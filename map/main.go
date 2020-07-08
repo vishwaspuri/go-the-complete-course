a400c52c4eb782cd523c019568d9e21c336b369e
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-08 21:46:44 +0530

    Maps in go

--- map/main.go
+++ map/main.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-	// fmt.Println(colors)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	printMap(colors)
+}
 
-	// First approach for creating an empty map
-	// var colors map[string]string
-	// Second approach for creating an empty map
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff"
-	// Deleting key value pairs
-	delete(colors, "white")
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hexcode for", color, "is", hex, "\b.")
+	}
 }
