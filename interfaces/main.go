0c25d753b9e62f3ddd198db03fcc1d190672b377
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-10 23:05:26 +0530

    After using interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,6 +5,10 @@ import "fmt"
 type englishBot struct{}
 type spanishBot struct{}
 
+type bot interface {
+	getGreeting() string
+}
+
 func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
@@ -12,17 +16,22 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.spanishBot)
-}
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.spanishBot)
+// }
 
 func (englishBot) getGreeting() string {
 	return "Hi! There."
 }
+
 func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
