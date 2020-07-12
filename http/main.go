a72b865ab1d9fd831b8c4c4bcdaa59a5b420b5b4
vishwaspuri <vishwaspuri09@gmail.com>
2020-07-12 20:36:18 +0530

    Baasic writer

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -12,5 +15,15 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp.Body.Close())
+	// fmt.Println(resp.Body.Close())
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+	lw := logWriter{}
+	// io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	return 1, nil
 }
